f85a392e88bb038e9fc4c1f2e05a75384339a11a
Subrata Mishra <71005032+subratami@users.noreply.github.com>
2025-06-05 17:55:37 +0000

    update

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -1,8 +1,13 @@
-
+import banner from ".//component/pexels-fauxels-3183132.jpg";
+import Advertisment from ".//component/w69uo7lk.png";
 function homepage() {
   return (
   <>
-  
+  <div className="banner">
+  <img className="banner-img" src={banner} alt="Banner Text"/> 
+  <img className="banner-Adv" src={Advertisment} alt="banner Add"/>
+</div>
+    
       </>
   );
   
