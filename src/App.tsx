57748639fb104929a8d716a7f942880e2d0ed96d
Subrata Mishra <71005032+subratami@users.noreply.github.com>
2025-06-05 16:17:16 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -157,59 +157,7 @@ function App() {
   <Route path="/Login" element={<Login />} />
   </Routes> 
   </Router>
-<footer className="footer">
-        <div>
-          <h2>Shopcart</h2>
-          <p>Customer Supports: 9834297810</p>
-          <p>Hilakandi Road, Silchar 788002</p>
-          <p>Shopcart@gmail.com</p>
-        </div>
-        <div>
-          <h3>Top Category</h3>
-          <ul>
-            <li>Computer & Laptop</li>
-            <li>SmartPhone</li>
-            <li>Headphone</li>
-            <li>Accessories</li>
-            <li>Cameras & Photo</li>
-            <li>TV & Speakers</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Quick Links</h3>
-          <ul>
-            <li>Shop Product</li>
-            <li>Shopping Cart</li>
-            <li>Wishlist</li>
-            <li>Compare</li>
-            <li>Track Order</li>
-            <li>Customer Help</li>
-          </ul>
-        </div>
-        <div className='download-app'>
-          <h3>Download App</h3>
-          <button>Get it now on Google Play</button>
-          <button>Get it now on App Store</button>
-        </div>
-       {/* <div>
-          <h3>Popular Tag</h3>
-          <ul>
-            <li>Game</li>
-            <li>iPhone</li>
-            <li>TV</li>
-            <li>Asus Laptops</li>
-            <li>Macbook</li>
-            <li>SSD</li>
-            <li>Graphics Card</li>
-            <li>Power Bank</li>
-            <li>Smart TV</li>
-            <li>Speaker</li>
-            <li>Tablet</li>
-            <li>Microwave</li>
-            <li>Samsung</li>
-          </ul>
-        </div> */}
-      </footer>
+
 </>
   );
 }
--- src/homepage.tsx
+++ src/homepage.tsx
@@ -3,11 +3,48 @@ import Advertisment from ".//component/w69uo7lk.png";
 import './homepage.css';
 function homepage() {
   return (
-  <><div className="banner">
+  <>
+  <div className="banner">
   <img className="banner-img" src={banner} alt="Banner Text"/> 
   <img className="banner-Adv" src={Advertisment} alt="banner Add"/>
-</div></>
-    
+</div>
+    <footer className="footer">
+        <div>
+          <h2>Shopcart</h2>
+          <p>Customer Supports: 9834297810</p>
+          <p>Hilakandi Road, Silchar 788002</p>
+          <p>Shopcart@gmail.com</p>
+        </div>
+        <div>
+          <h3>Top Category</h3>
+          <ul>
+            <li>Computer & Laptop</li>
+            <li>SmartPhone</li>
+            <li>Headphone</li>
+            <li>Accessories</li>
+            <li>Cameras & Photo</li>
+            <li>TV & Speakers</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Quick Links</h3>
+          <ul>
+            <li>Shop Product</li>
+            <li>Shopping Cart</li>
+            <li>Wishlist</li>
+            <li>Compare</li>
+            <li>Track Order</li>
+            <li>Customer Help</li>
+          </ul>
+        </div>
+        <div className='download-app'>
+          <h3>Download App</h3>
+          <button>Get it now on Google Play</button>
+          <button>Get it now on App Store</button>
+        </div>
+      </footer>
+      </>
   );
+  
 }
 export default homepage;
