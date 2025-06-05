054d1e151e900dc79dbfe5c8257ce31bf9062962
subratami <subratamishra213@gmail.com>
2025-06-05 20:24:34 +0530

    Initial commit

---
+++ src/component/login.tsx
@@ -0,0 +1,33 @@
+
+import './login.css'; 
+
+const Login = () => {
+  return (
+    <>
+    
+    <div className="login-container">
+      <div className="auth-form">
+        <h2>Log In</h2>
+        <form>
+          <label>Email Address</label>
+          <input type="email" required />
+
+          <label>Password</label>
+          <input type="password" required />
+
+          <button type="submit">LOG IN</button>
+          <p><a href="#">Forget Password?</a></p>
+        </form>
+        <div className="social-login">
+          <p>or</p>
+          <button className="google-login">Login with Google</button>
+          <button className="apple-login">Login with Apple</button>
+        </div>
+      </div>
+    </div>
+      
+</>
+  )
+};
+
+export default Login
---
+++ src/homepage.tsx
@@ -0,0 +1,13 @@
+import banner from ".//component/pexels-fauxels-3183132.jpg";
+import Advertisment from ".//component/w69uo7lk.png";
+import './homepage.css';
+function homepage() {
+  return (
+  <><div className="banner">
+  <img className="banner-img" src={banner} alt="Banner Text"/> 
+  <img className="banner-Adv" src={Advertisment} alt="banner Add"/>
+</div></>
+    
+  );
+}
+export default homepage;
