3fe6617ae9d76d7aeab77d74ce83d5e2b7c53401
Subrata Mishra <71005032+subratami@users.noreply.github.com>
2025-06-05 16:35:08 +0000

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 // Make sure the file exists as Homepage.tsx or Homepage.jsx in the same folder, or update the path accordingly.
 import Homepage from './homepage';
 import Login from ".//component/login";
+import banner from ".//component/pexels-fauxels-3183132.jpg";
+import Advertisment from ".//component/w69uo7lk.png";
+import './homepage.css';
 function App() {
   return (
     <>
@@ -157,7 +160,45 @@ function App() {
   <Route path="/Login" element={<Login />} />
   </Routes> 
   </Router>
-
+<div className="banner">
+  <img className="banner-img" src={banner} alt="Banner Text"/> 
+  <img className="banner-Adv" src={Advertisment} alt="banner Add"/>
+</div>
+    <footer className="footer">
+        <div>
+          <h2>Shopcart</h2>
+          <p>Customer Supports: 9834297810</p>
+          <p>Hilakandi Road, Silchar 788002</p>
+          <p>Shopcart@gmail.com</p>
+        </div>
+        <div>
+          <h3>Top Category</h3>
+          <ul>
+            <li>Computer & Laptop</li>
+            <li>SmartPhone</li>
+            <li>Headphone</li>
+            <li>Accessories</li>
+            <li>Cameras & Photo</li>
+            <li>TV & Speakers</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Quick Links</h3>
+          <ul>
+            <li>Shop Product</li>
+            <li>Shopping Cart</li>
+            <li>Wishlist</li>
+            <li>Compare</li>
+            <li>Track Order</li>
+            <li>Customer Help</li>
+          </ul>
+        </div>
+        <div className='download-app'>
+          <h3>Download App</h3>
+          <button>Get it now on Google Play</button>
+          <button>Get it now on App Store</button>
+        </div>
+      </footer>
 </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './App.css'
-import Homepage from './homepage.tsx'
+//import Homepage from './homepage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App/>
-    <Homepage />
+    
   </StrictMode>,
 )
