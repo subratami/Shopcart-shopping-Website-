fc2be69e9ff83b0831cf0424b055cc416143df2e
Subrata Mishra <71005032+subratami@users.noreply.github.com>
2025-06-06 14:00:23 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-//import * as React from "react";
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-//import Header from "./component/header";
 import Homepage from './component/homepage';
-//import Footer from './/component/footer'
 import Layout from "./component/layout";
 import Login from "./component/login";
 function App() {
