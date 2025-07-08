3519eefc034c90c36b98d6071c1ab2c69283ff8b
subratami <subratamishra213@gmail.com>
2025-07-08 21:40:06 +0530

    update cart ui

--- src/component/cart.tsx
+++ src/component/cart.tsx
@@ -22,17 +22,17 @@ const Cart: React.FC = () => {
         {cart.map((item: CartItem) => {
           const id = item.product_id || item._id; // <-- Add this line
           return (
-            <li key={id} style={{ marginBottom: "1em" }}>
-              <div>
+            <li key={id} style={{ marginBottom: "0.5em" }}>
+              <div style={{display: "block", flexDirection: "column"}}>
+                <span style={{width:"100px", height: "100px"}}><img src="#" alt="Product" style={{width:"100px", height: "100px"}} /> </span></div>
+                <div>
                 <strong>
                   {item.Brand ? item.Brand : ""}&nbsp;&nbsp;{item.Model ? item.Model : ""}&nbsp;&nbsp;{item.Color ? item.Color: ""}
                 </strong>
                 <br />
                 {item.Memory ? item.Memory : ""}&nbsp;&nbsp;{item.Storage ? item.Storage : ""}
-                
-                </div>
-                <div>
-                 <span style={{backgroundColor: 'gray', padding: '2px 4px 4px 4px', borderRadius: '4px'}}> <button
+                <br/>
+                 <span style={{padding: '2px 4px 4px 4px', borderRadius: '4px', display: 'flex', flexDirection: 'row'}}> <button
                     onClick={() => handleQuantityChange(id, item.quantity - 1)}
                     /*disabled={item.quantity <= 1} */
                     style={{ width: 30, backgroundColor:'black', color:'white' }}
@@ -46,7 +46,7 @@ const Cart: React.FC = () => {
                     onChange={(e) =>
                       updateCartItem(id, parseInt(e.target.value)) 
                     }
-                    style={{ width: "30px", padding: '1px 4px', textAlign: 'center' }}
+                    style={{ width: "30px", padding: '0px 4px', textAlign: 'center' }}
                   />
                   <button
                     onClick={() => handleQuantityChange(id, item.quantity + 1)}
@@ -54,10 +54,9 @@ const Cart: React.FC = () => {
                   >
                     +
                   </button><br/>
-                  </span>
-                  <span >
+                
                   <button
-                  style={{ marginLeft: "2em", marginTop: "1em" }}
+                  style={{ marginLeft: "2em" }}
                   onClick={() => removeFromCart(id)}
                 >
                   Delete
@@ -73,6 +72,14 @@ const Cart: React.FC = () => {
           );
         })}
       </ul>
+      <div style={{ textAlign: "right", fontWeight: "bold", margin: "1em 0" }}>
+  Subtotal: ₹
+  {cart.reduce(
+    (sum, item) =>
+      sum + (item["Selling Price"] ? item["Selling Price"] * item.quantity : 0),
+    0
+  )}
+</div>
       <div className="cart-bottom">
       <button onClick={clearCart} style={{ marginRight: "1em" }}>
         Clear Cart
@@ -85,95 +92,3 @@ const Cart: React.FC = () => {
 };
 
 export default Cart;
-{/*import React from "react";
-import { useCart,type CartItem } from "./CartContext";
-import "./cart.css"
-
-const Cart: React.FC = () => {
-  const { cart, loading, error, removeFromCart, updateCartItem, clearCart, checkout } = useCart();
-
-  if (loading) return <div>Loading cart...</div>;
-  if (error) return <div style={{ color: "red" }}>{error}</div>;
-  if (!cart.length) return <div>Your cart is empty.</div>;
-  
-  const handleQuantityChange = (_id: string, quantity: number) => {
-    if (quantity < 1) return;
-    updateCartItem(_id, quantity);
-  };
-
-  return (
-    <div className="cartlist">
-      <h2>Your Cart</h2>
-      <ul>
-        {cart.map((item: CartItem) => (
-          <li key={item._id} style={{ marginBottom: "1em" }}>
-            <div>
-              <strong>
-                {item.Brand ? item.Brand : ""} {item.Model ? item.Model : ""}
-              </strong>
-              <br />
-              <div style={{backgroundColor: 'gray', padding: '4px 8px', borderRadius: '4px'}}>
-            <button
-             onClick={() => handleQuantityChange(item._id, item.quantity - 1)}
-      style={{ width: 30, backgroundColor:'#f4a7a7' }}
-            >
-              -
-            </button>
-            <input
-              type="number"
-              min="1"
-              value={item.quantity}
-              onChange={(e) =>
-                handleQuantityChange(item._id, parseInt(e.target.value))
-              }
-              style={{ width: "30px", padding: '1px 4px', textAlign: 'center' }}
-            />
-    
-            <button
-      onClick={() => handleQuantityChange(item._id, item.quantity + 1)}
-      style={{ width: 30, backgroundColor: '#87df87'}}
-    >
-      +
-    </button>
-            </div>
-              <button
-                style={{ marginLeft: "1em" }}
-                onClick={() => removeFromCart(item._id)}
-              >
-                Remove
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <button onClick={clearCart} style={{ marginRight: "1em" }}>
-        Clear Cart
-      </button>
-      <button onClick={checkout}>Checkout</button>
-    </div>
-  );
-};
-
-export default Cart;
-
-
-{/*import { useCart } from "../component/CartContext";
-import "./cart.css"; // Assuming you have a CSS file for styling
-const CartPage = () => {
-  const { cart } = useCart();
-
-  return (
-    <div className="cartlist">
-      <ul>
-        {cart.map((item, idx) => (
-          <li key={idx}>
-            {item.Brand} &nbsp; {item.Model} &nbsp; {item.Color} &nbsp; {item.Memory} &nbsp; {item.Storage}
-            {/* Add other fields as needed 
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default CartPage; */}
