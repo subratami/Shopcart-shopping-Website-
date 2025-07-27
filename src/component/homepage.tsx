f62dfecac09d71a1daa868cd7836e1915d536df0
subratami <subratamishra213@gmail.com>
2025-07-27 19:05:58 +0530

    update homepage

--- src/component/homepage.tsx
+++ src/component/homepage.tsx
@@ -54,7 +54,7 @@ function Homepage() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
-              /> 
+              />
           </motion.li>
 
           <motion.li
@@ -110,23 +110,3 @@ function Homepage() {
 }
 
 export default Homepage;
-
-
-{/*import banner from "./pexels-fauxels-3183132.jpg";
-import Advertisment from "./w69uo7lk.png";
-import './homepage.css';
-import ShopByCatgories from '../component/ShopByCategories';
-function homepage() {
-  return (
-  <>
-  <div className="banner">
-  <img className="banner-img" src={banner} alt="Banner Text"/> 
-  <img className="banner-Adv" src={Advertisment} alt="banner Add"/>
-  <ShopByCatgories />
-</div>
-    
-  </>
-  );
-  
-}
-export default homepage; */}
