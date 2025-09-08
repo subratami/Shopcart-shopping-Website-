483b6e68588e1de384aca2d9f19198b9e82c03ee
subratami <subratamishra213@gmail.com>
2025-09-08 15:10:01 +0530

    app update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Login from "./component/login";
 import Header from "./component/header";
 import Footer from "./component/footer";
 import Signup from "./component/signup";
-import Dashboard from './component/dashboard';
+//import Dashboard from './component/dashboard';
 import ProtectedRoute from './utils/ProtectedRoute';
 import PublicRoute from './utils/PublicRoute';
 import { useState,useEffect } from 'react'
@@ -44,7 +44,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
           <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          {/*<Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />*/}  
           <Route path="/search" element={<ProductList searchQuery={searchQuery} />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/wishlist" element={<Wishlist />} />
